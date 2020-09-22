e02883356a6b5bf4aae9e534f9f5850ec7061dcb
VivaEcha838 <68964649+VivaEcha838@users.noreply.github.com>
2020-09-21 19:10:22 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Database-Car-Racing-Class
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, bgImage;
+
+var gameState = 0;
+
+var playerCount;
+
+var form, player, game;
+
+var database;
+
+function setup(){
+    createCanvas(500,500);
+
+database = firebase.database();
+
+game = new Game();
+game.getState();
+game.start();
+
+}
+
+function draw(){
+  
+    
+}
+
+
